bd988d8ca2b097914fd1ee1ed18c1be7f0c5442d
Pablo Aguirre <pabloaguirrenck@gmail.com>
2023-12-17 19:55:11 -0600

    Updated Guided Project

--- data-science/the-state-of-our-world-in-2023/resources/indicator_list_updater.py
+++ data-science/the-state-of-our-world-in-2023/resources/indicator_list_updater.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import openpyxl
 import os
+from pathlib import Path
 
-# Load Excel file
-df = pd.read_excel("Indicator_List.xlsx", sheet_name="Indicators")
+# Get the absolute path to the directory where this script is located
+script_directory = Path(__file__).parent
 
-# Get cols
+# Define the path to the Excel file and the output CSV file
+excel_file_path = script_directory / "resources" / "Indicator_List.xlsx"
+csv_file_path = script_directory / "Indicator_List.csv"
+
+# Load the Excel file
+df = pd.read_excel(excel_file_path, sheet_name="Indicators")
+
+# Get the relevant columns
 target_cols = ["Metric Code", "Segment", "Subject", "Metric", "Source"]
 df = df[target_cols]
 
-# Write
-df.to_csv("Indicator_List.csv", index=False)
+# Write to the CSV file
+df.to_csv(csv_file_path, index=False)
